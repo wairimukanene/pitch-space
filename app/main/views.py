c12e73023a16d3747988cf5b0640fb7d7c37cddb
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-09 22:43:25 +0300

    commit5-added index router for the title

---
+++ app/main/views.py
@@ -0,0 +1,14 @@
+from flask import render_template,request,redirect,url_for,abort,flash
+from ..import main
+
+
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    
+    title = 'Pitch Deck'
+    
+    return render_template('index.html', title = title)
