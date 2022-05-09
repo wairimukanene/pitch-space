a7617455e5f5043ee703808355f674d85151bf53
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-09 22:52:08 +0300

    commit6-on view.py added main router for logged in

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,10 @@
 from flask import render_template,request,redirect,url_for,abort,flash
 from ..import main
+from flask_login import login_required,current_user
+from ..models import User,Pitch,Comment,Upvote,Downvote
+from .forms import UpdateProfile,AddPitch,CommentForm,UpvoteForm,Downvote
+from .. import db,photos
+from flask.views import View,MethodView
 
 
 @main.route('/')
@@ -12,3 +17,27 @@ def index():
     title = 'Pitch Deck'
     
     return render_template('index.html', title = title)
+
+
+@main.route('/loggedin')
+def loggedin():
+
+    title = 'Pitch Deck'
+
+    return render_template('loggedin.html',title =title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    title = f'{uname} Profile'
+    return render_template("profile/profile.html", user = user, title = title)
+
+
+
+
+
+
