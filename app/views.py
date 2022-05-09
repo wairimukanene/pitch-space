8aae2944ecf6c0c193e1172e460204d1a247d252
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-09 21:32:25 +0300

    commit1-created app folders and files

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from flask import app
+from flask import render_template,request,redirect,url_for
+from app import app
+
+
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    
+    title = 'Pitch Space'
+    
+    return render_template('index.html', title = title)
