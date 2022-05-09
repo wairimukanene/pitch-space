ec8a969df64ad139d5a6015a9a669aedc59c5783
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-09 22:59:02 +0300

    commit7-added profile and update profile functionality

--- app/main/views.py
+++ app/main/views.py
@@ -14,7 +14,7 @@ def index():
     View root page function that returns the index page and its data
     '''
     
-    title = 'Pitch Deck'
+    title = 'Pitch Space'
     
     return render_template('index.html', title = title)
 
@@ -22,7 +22,7 @@ def index():
 @main.route('/loggedin')
 def loggedin():
 
-    title = 'Pitch Deck'
+    title = 'Pitch Space'
 
     return render_template('loggedin.html',title =title)
 
@@ -35,6 +35,41 @@ def profile(uname):
 
     title = f'{uname} Profile'
     return render_template("profile/profile.html", user = user, title = title)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+        
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+    title = 'Update | Profile'
+    return render_template('profile/update.html',form =form, title = title)
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+        
+        
 
 
 
