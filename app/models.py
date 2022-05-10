6f2f44aa4576f67d65c4462f4cf4305eb96c379c
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-10 12:42:12 +0300

    commit24-repeated the same process for class pitch

--- app/models.py
+++ app/models.py
@@ -36,6 +36,33 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User{self.username}'
+      
+      
+class Pitch(db.Model):
+  
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    description = db.Column(db.String(), index = True)
+    title = db.Column(db.String())
+    pitcher = db.Column(db.String())
+    category = db.Column(db.String(255), nullable=False)
+    comments = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    upvotes = db.relationship('Upvote', backref = 'pitch', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref = 'pitch', lazy = 'dynamic')
+
+    
+    @classmethod
+    def get_pitches(cls, id):
+        pitches = Pitch.query.order_by(pitch_id=id).desc().all()
+        return pitches
+
+    def __repr__(self):
+        return f'Pitch {self.description}'
+
+    
+
 
     
   
