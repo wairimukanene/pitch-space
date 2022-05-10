e33205ed6a96ed1ec7546803e2a7f43f585cd8f9
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-10 10:38:02 +0300

    commit17-added classes instance in form.py

---
+++ app/main/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField,TextAreaField,StringField,SelectField
+from wtforms.validators import Required
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Add or Update your bio ',validators = [Required()])
+    submit = SubmitField('Submit')
+    
+    
+class AddPitch(FlaskForm):
+    pitcher = StringField("Submitted By: Your Name ...", validators = [Required()])
+    title = StringField("Pitch Title", validators = [Required()])
+    category = SelectField("What category are you submitting to?", choices=[("twitter", "Twitter"), ( "elevator", "Elevator"), ("competition", "Competition"), ("investor", "Investor")],validators=[Required()])
+    description = TextAreaField('What pitch do you want to share?',validators = [Required()] )
+    submit = SubmitField('Submit') 
+    
+class CommentForm(FlaskForm):
+
+    description = TextAreaField('Add a comment',validators = [Required()] )
+    submit = SubmitField('Submit')  
+    
+class UpvoteForm(FlaskForm):
+	submit = SubmitField()
+
+
+class Downvote(FlaskForm):
+	submit = SubmitField()
