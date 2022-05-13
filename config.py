55d48d989966880a02702a55f97491ea7a231dde
wairimu.Kanene <mercykanene254@gmail.com>
2022-05-13 17:16:53 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 
 import os
 
+
 class Config:
     '''
     General configuration parent class
@@ -9,24 +10,23 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wairimu:wairimu12@localhost/space'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     
+    
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") 
+    MAIL_USERNAME = 'mercykanene254@gmail.com'
+    MAIL_PASSWORD = 'naivasha2020'
+    
     
 
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    DEBUG = True
+    
+    
 
 class DevConfig(Config):
     '''
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export SECRET_KEY=37008163
-export MAIL_USERNAME=mercykanene@gmail.com
-export MAIL_PASSWORD=naivasha2020
+export SECRET_KEY='1234'
+export MAIL_USERNAME='mercykanene@gmail.com'
+export MAIL_PASSWORD='naivasha2020'
 
 python3.8 manager.py server
